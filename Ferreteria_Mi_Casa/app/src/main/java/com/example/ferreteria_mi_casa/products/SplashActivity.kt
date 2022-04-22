f17fe229d2240c65f3a78b3f8491dc8a4468917b
Diego Sandoval <sandovaldiego21@gmail.com>
2022-04-21 22:46:15 -0600

    Catalogo e informacion

---
+++ Ferreteria_Mi_Casa/app/src/main/java/com/example/ferreteria_mi_casa/products/SplashActivity.kt
@@ -0,0 +1,15 @@
+package com.example.ferreteria_mi_casa.products
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ferreteria_mi_casa.Home
+import com.example.ferreteria_mi_casa.MainActivity
+import com.example.ferreteria_mi_casa.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+startActivity(Intent(this,MainActivity::class.java))
+    }
+}
