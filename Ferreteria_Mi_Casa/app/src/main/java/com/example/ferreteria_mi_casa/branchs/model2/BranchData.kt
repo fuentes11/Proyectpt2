26c29afa3de9ddffd3af448edb1dab4bdc19f9f6
Diego Sandoval <sandovaldiego21@gmail.com>
2022-04-22 22:22:13 -0600

    Data y db BRANCH

--- Ferreteria_Mi_Casa/app/src/main/java/com/example/ferreteria_mi_casa/branchs/model2/BranchData.kt
+++ Ferreteria_Mi_Casa/app/src/main/java/com/example/ferreteria_mi_casa/branchs/model2/BranchData.kt
@@ -1,5 +1,18 @@
 package com.example.ferreteria_mi_casa.branchs.model2
 
 class BranchData {
+    var name :String? = null
+    var info:String? = null
+    var image:String? = null
+    constructor(){}
 
+    constructor(
+        name:String?,
+        info:String?,
+        image:String?
+    ){
+        this.name = name
+        this.info = info
+        this.image = image
+    }
 }
--- Ferreteria_Mi_Casa/app/src/main/java/com/example/ferreteria_mi_casa/branchs/model2/branchDb.kt
+++ Ferreteria_Mi_Casa/app/src/main/java/com/example/ferreteria_mi_casa/branchs/model2/branchDb.kt
@@ -1,4 +1,38 @@
 package com.example.ferreteria_mi_casa.branchs.model2
 
+import android.content.Context
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.ferreteria_mi_casa.R
+
 class branchDb {
+    fun getProgessDrawable(c: Context): CircularProgressDrawable {
+        return CircularProgressDrawable(c).apply {
+            strokeWidth = 5f
+            centerRadius = 40f
+            start()
+        }
+
+    }
+
+    /**set Images*/
+    fun ImageView.loadImage(uri:String?, progressDawable: CircularProgressDrawable){
+
+        val option = RequestOptions().placeholder(progressDawable)
+            .error(R.mipmap.ic_launcher)
+
+        Glide.with(context)
+            .setDefaultRequestOptions(option)
+            .load(uri)
+            .into(this)
+
+    }
+
+    @BindingAdapter("android:imageUrl")
+    fun loadImage(view: ImageView, url:String){
+        view.loadImage(url, getProgessDrawable(view.context))
+    }
 }
