67eefef1809039833f86763f9ae26b6e18ebe5bf
jason <peke3025@gmail.com>
2022-04-22 22:30:16 -0600

    Gitignore

--- Ferreteria_Mi_Casa/app/src/main/java/com/example/ferreteria_mi_casa/Mainbranch.kt
+++ Ferreteria_Mi_Casa/app/src/main/java/com/example/ferreteria_mi_casa/Mainbranch.kt
@@ -2,11 +2,63 @@ package com.example.ferreteria_mi_casa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ferreteria_mi_casa.R
+import com.example.ferreteria_mi_casa.branchs.BranchAdapter
+import com.example.ferreteria_mi_casa.branchs.model2.BranchData
+import com.example.ferreteria_mi_casa.branchs.model2.branchDb
+import com.example.ferreteria_mi_casa.products.Adapter
+import com.example.ferreteria_mi_casa.products.model.ProductData
+import com.google.firebase.database.*
+import kotlinx.android.synthetic.main.activity_main_products.*
+import kotlinx.android.synthetic.main.activity_mainbranch.*
 
 class Mainbranch : AppCompatActivity() {
+    lateinit var mmDataBase: DatabaseReference
+    private lateinit var BranchList:ArrayList<BranchData>
+    private lateinit var mmAdapter: BranchAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mainbranch)
+        /**initialized*/
+        BranchList = ArrayList()
+        mmAdapter = BranchAdapter(this, BranchList )
+        recyclerbranch.layoutManager = LinearLayoutManager(this)
+        recyclerbranch.setHasFixedSize(true)
+        // recyclerAnimals.adapter = mAdapter
+        /**getData firebase*/
+        getProductsData()
+
     }
+    /**ok now create new activity*/
+
+
+    private fun getProductsData() {
+
+        mmDataBase = FirebaseDatabase.getInstance().getReference("Sucursales")
+        mmDataBase.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()){
+                    for (animalSnapshot in snapshot.children){
+                        val animal = animalSnapshot.getValue(BranchData::class.java)
+                        BranchList.add(animal!!)
+                    }
+                    recyclerbranch.adapter = mmAdapter
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(this@Mainbranch,
+                    error.message, Toast.LENGTH_SHORT).show()
+            }
+
+        })
+
+
+    }
+
+
+
+
 }
