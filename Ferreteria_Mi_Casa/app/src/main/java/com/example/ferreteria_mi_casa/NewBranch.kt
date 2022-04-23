b2f743cc1fb2f0bf63a2b07c56ffee75f791ef45
Diego Sandoval <sandovaldiego21@gmail.com>
2022-04-22 22:34:01 -0600

    new BRANCH

---
+++ Ferreteria_Mi_Casa/app/src/main/java/com/example/ferreteria_mi_casa/NewBranch.kt
@@ -0,0 +1,25 @@
+package com.example.ferreteria_mi_casa
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_new.*
+import kotlinx.android.synthetic.main.activity_new_branch.*
+
+class NewBranch : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new)
+
+        /**get Data*/
+        val BranchIntent = intent
+        val BranchName = BranchIntent.getStringExtra("name")
+        val Branchinfo = BranchIntent.getStringExtra("info")
+        val BranchImage = BranchIntent.getStringExtra("image")
+
+        /**call text and images*/
+        name2.text = BranchName
+        info2.text = Branchinfo
+        image2.loadImage(BranchImage, getProgessDrawable(this))
+    }
+    /**ok now run it */
+}
